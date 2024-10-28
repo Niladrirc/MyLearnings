3814c07be83af53a7d5d1abb3ff6f12a02a98397
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-10-28 18:34:15 +0530

    Update BinarySearch.java

--- 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/1: Array/Java/src/easy/BinarySearch.java
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/1: Array/Java/src/easy/BinarySearch.java
@@ -3,8 +3,8 @@ package src.easy;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = {-1,0,3,5,9,12};
-        int target = 2;
-        System.out.println("Index: "+search(nums, target));
+        int target = 9;
+        System.out.println("Index: "+search(nums, 0, nums.length-1, target));
     }
 
     private static int search(int[] nums, int target) {
@@ -23,6 +23,8 @@ public class BinarySearch {
 
     private static int search(int[] nums, int start, int end, int target) {
         int mid = (start+end)/2;
+        if (start > end)
+            return -1;
         if (nums[mid] == target)
             return mid;
 
