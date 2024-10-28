5c73c8f10ffafc6d72395f268bc0264fc696b8ba
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-10-28 18:16:34 +0530

    Update BaseballGame.java

--- 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/1: Array/Java/src/easy/BaseballGame.java
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/1: Array/Java/src/easy/BaseballGame.java
@@ -5,23 +5,31 @@ import java.util.List;
 
 public class BaseballGame {
     public static void main(String[] args) {
-        String[] ops = {"5","2","C","D","+"};
+        String[] ops = {"5","-2","4","C","D","9","+","+"};
 
         System.out.println("Output: " + calPoints(ops));
     }
 
     private static int calPoints(String[] ops) {
         List<Integer> record = new ArrayList<>();
-
+        int sum = 0;
         for (String op : ops) {
             switch (op) {
-                case "+" -> record.add(record.get(record.size() - 2) + record.get(record.size() - 1));
-                case "D" -> record.add(record.get(record.size() - 1) * 2);
-                case "C" -> record.remove(record.size() - 1);
-                default -> record.add(Integer.parseInt(op));
+                case "+" -> {
+                    sum += (record.get(record.size() - 1) + record.get(record.size() - 2));
+                    record.add(record.get(record.size() - 1) + record.get(record.size() - 2));
+                }
+                case "D" -> {
+                    sum += record.get(record.size() - 1) * 2;
+                    record.add(record.get(record.size() - 1) * 2);
+                }
+                case "C" -> sum -= record.remove(record.size() - 1);
+                default -> {
+                    sum += Integer.parseInt(op);
+                    record.add(Integer.parseInt(op));
+                }
             }
         }
-
-        return record.stream().mapToInt(Integer::intValue).sum();
+        return sum;
     }
 }
