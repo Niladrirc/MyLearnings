7442f3e68867708ca93d752a8c8db114780821e5
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-10-30 09:33:54 +0530

    Update MaximumGoodSubarraySum.java

--- 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/1: Array/Java/src/medium/MaximumGoodSubarraySum.java
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/1: Array/Java/src/medium/MaximumGoodSubarraySum.java
@@ -11,19 +11,36 @@ public class MaximumGoodSubarraySum {
         System.out.println("Maximum Good Subarray Sum: " + maximumSubarraySum(nums, k));
     }
 
+    /**
+     * Requirements:
+     *      Hashmap -> stores every element in the array (as key) and the corresponding sum of all elements before that (as prefixSum).
+     * LOGIC: For any value in the array
+     *          1. check if the value already exists in hashmap
+     *              1.1 If exists -> compare the corresponding prefixSum with current prefixSum and store the lesser value.
+     *                  1.1.1 This ensures that if duplicates are present, storing lesser prefixSum gets us larger maxSum
+     *              1.2 If does not exist -> add it to the hashmap with current prefixSum
+     *          2. For the currentElement, update the prefixSum
+     *          3. Get complement1 & complement2
+     *          4. Check which exists in hashmap,
+     *              4.1 For anyone that exists: we compare the current prefixSum i.e. sum of all elements till current element and complement's prefixSum i.e. sum of all elements till that (excluding it)
+     *                      this will give the sum from the complement to currentElement
+     *                      and as we only store minimum prefixSum -> we get the max sum
+     */
     public static long maximumSubarraySum(int[] nums, int k) {
         Map<Integer, Long> valToMinPrefixSum = new HashMap<>();
         long maxSum = Long.MIN_VALUE, prefixSum = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            if (valToMinPrefixSum.getOrDefault(nums[i], Long.MAX_VALUE) > prefixSum) {
-                valToMinPrefixSum.put(nums[i], prefixSum);
+        for (int currentElement : nums) {
+            if (valToMinPrefixSum.getOrDefault(currentElement, Long.MAX_VALUE) > prefixSum) {
+                valToMinPrefixSum.put(currentElement, prefixSum);
             }
-            prefixSum += nums[i];
-            if (valToMinPrefixSum.containsKey(nums[i] + k)) {
-                maxSum = Math.max(maxSum, prefixSum - valToMinPrefixSum.get(nums[i] + k));
+            prefixSum += currentElement;
+            int complement1 = currentElement + k;
+            int complement2 = currentElement - k;
+            if (valToMinPrefixSum.containsKey(complement1)) {
+                maxSum = Math.max(maxSum, prefixSum - valToMinPrefixSum.get(complement1));
             }
-            if (valToMinPrefixSum.containsKey(nums[i] - k)) {
-                maxSum = Math.max(maxSum, prefixSum - valToMinPrefixSum.get(nums[i] - k));
+            if (valToMinPrefixSum.containsKey(complement2)) {
+                maxSum = Math.max(maxSum, prefixSum - valToMinPrefixSum.get(complement2));
             }
         }
         return maxSum == Long.MIN_VALUE ? 0 : maxSum;
