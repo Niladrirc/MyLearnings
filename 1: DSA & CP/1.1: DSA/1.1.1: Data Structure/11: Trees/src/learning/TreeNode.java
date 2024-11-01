c46ff64af583c9d540749cfcad640ee506dc1195
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-11-01 18:59:26 +0530

    Create TreeNode.java

---
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/11: Trees/src/learning/TreeNode.java
@@ -0,0 +1,18 @@
+package src.learning;
+
+public class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    public TreeNode() {}
+
+     public TreeNode(int data) {
+        this.data = data;
+     }
+
+     public TreeNode(int data, TreeNode left, TreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
