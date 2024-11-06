f422ffa75a62eb2ad0e5d0d8b1a3f2e8908b95a3
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-11-06 14:23:06 +0530

    Update MergeTwoSortedLists.java

--- 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/5: Linked List/src/easy/MergeTwoSortedLists.java
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/5: Linked List/src/easy/MergeTwoSortedLists.java
@@ -11,20 +11,20 @@ public class MergeTwoSortedLists {
         list2.next = new ListNode(3);
         list2.next.next = new ListNode(4);
 
-        ListNode list3 = mergeTwoLists(list1, list2);
+        ListNode list3 = mergeTwoListsIterative(list1, list2);
         while (list3 != null) {
-            System.out.println(list3.val);
+            System.out.print(list3.val+ " ");
             list3 = list3.next;
         }
     }
 
-    private static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    private static ListNode mergeTwoListsRecursive(ListNode list1, ListNode list2) {
         if (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                list1.next = mergeTwoLists(list1.next, list2);
+                list1.next = mergeTwoListsRecursive(list1.next, list2);
                 return list1;
             } else {
-                list2.next = mergeTwoLists(list1, list2.next);
+                list2.next = mergeTwoListsRecursive(list1, list2.next);
                 return list2;
             }
         }
@@ -32,5 +32,37 @@ public class MergeTwoSortedLists {
             return list2;
         }
         return list1;
+    }
+
+    private static ListNode mergeTwoListsIterative(ListNode list1, ListNode list2) {
+        ListNode list3 = new ListNode(), last = list3;
+        if (list1.val < list2.val) {
+            list3 = last = list1;
+            list1 = list1.next;
+        } else {
+            list3 = last = list2;
+            list2 = list2.next;
+        }
+        last.next = null;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                last.next = list1;
+                last = list1;
+                list1 = list1.next;
+            } else {
+                last.next = list2;
+                last = list2;
+                list2 = list2.next;
+            }
+            last.next = null;
+        }
+        if (list1 != null) {
+            last.next = list1;
+        } else {
+            last.next = list2;
+        }
+
+        return list3;
     }
 }
