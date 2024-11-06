c603113f04f16108dd8a4f894371bd195cd7789b
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-11-06 09:17:33 +0530

    Create MergeTwoSortedLists.java

---
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/5: Linked List/src/easy/MergeTwoSortedLists.java
@@ -0,0 +1,46 @@
+package src.easy;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public void add(int val) {
+    }
+}
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        ListNode list2 = new ListNode(1);
+
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode list3 = mergeTwoLists(list1, list2);
+        while (list3 != null) {
+            System.out.println(list3.val);
+            list3 = list3.next;
+        }
+    }
+
+    private static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+        if (list1 == null) {
+            return list2;
+        }
+        return list1;
+    }
+}
