a2d6c0df1fc71a891d505660674f3c7231c4dded
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-11-13 20:11:29 +0530

    GCD of Strings

--- 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/1: Array/src/java/general/GCD.java
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/1: Array/src/java/general/GCD.java
@@ -15,13 +15,13 @@ public class GCD {
         System.out.println("\nGCD of " + number1 + " and " + number2 + " is " + gcd);
     }
 
-    private static int gcdR(int number1, int number2) {
+    public static int gcdR(int number1, int number2) {
         if ( number1 == 0)
             return number2;
         return gcdR(number2 % number1, number1);
     }
 
-    private static int gcdI(int number1, int number2) {
+    public static int gcdI(int number1, int number2) {
         while (number2 != 0) {
             int temp = number2;
             number2 = number1 % number2;
