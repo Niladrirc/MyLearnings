e4a9333f2505a3be6ffd6929fa183d9f8e09c4c2
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-11-28 20:06:28 +0530

    Update TreeNode.java

--- 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/11: Trees/src/learning/TreeNode.java
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/11: Trees/src/learning/TreeNode.java
@@ -5,14 +5,6 @@ public class TreeNode {
     TreeNode left, right;
 
     public TreeNode() {}
-
-     public TreeNode(int data) {
-        this.data = data;
-     }
-
-     public TreeNode(int data, TreeNode left, TreeNode right) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
+    public TreeNode(int data) { this.data = data; this.left = this.right = null; }
+    public TreeNode(int data, TreeNode left, TreeNode right) { this.data = data; this.left = left; this.right = right; }
 }
