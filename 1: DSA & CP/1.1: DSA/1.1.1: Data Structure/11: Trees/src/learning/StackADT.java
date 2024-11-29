e4d7f8fb9861a6a673b71c7ee8f60921e41b65ec
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-11-29 15:17:09 +0530

    Create StackADT.java

---
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/11: Trees/src/learning/StackADT.java
@@ -0,0 +1,48 @@
+package src.learning;
+
+public class StackADT<T> {
+    static class Node<E> {
+
+        private final E data;
+        private Node<E> next;
+        public Node(E data) { this.data = data; }
+
+    }
+
+    private Node<T> top;
+    private int size;
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public void push(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (isEmpty()) {
+            top = newNode;
+            size = 1;
+        } else {
+            Node<T> temp = top;
+            while(temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            size += 1;
+        }
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return null;
+        }
+        T data = top.data;
+        top = top.next;
+        size -= 1;
+        return data;
+    }
+}
