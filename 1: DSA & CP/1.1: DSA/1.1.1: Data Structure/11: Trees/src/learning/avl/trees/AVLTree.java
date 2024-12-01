de9fe84157b3424f7306652c3735e77173dd0793
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-01 17:20:46 +0530

    Update AVLTree.java

--- 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/11: Trees/src/learning/avl/trees/AVLTree.java
+++ 1: DSA & CP/1.1: DSA/1.1.1: Data Structure/11: Trees/src/learning/avl/trees/AVLTree.java
@@ -3,13 +3,18 @@ package src.learning.avl.trees;
 import src.learning.TreeNode;
 
 public class AVLTree {
-    private TreeNode root;
+    private AVLNode root;
 
     public int treeHeight(AVLNode node) {
         int heightLeft = (node != null && node.left != null) ? node.left.height : 0;
         int heightRight = (node != null && node.right != null) ? node.right.height : 0;
         return Math.max(heightLeft, heightRight)+1;
     }
+
+    private int getBalanceFactor(AVLNode node) {
+        return treeHeight(node.left) - treeHeight(node.right);
+    }
+
 
     public boolean isEmpty() {
         return root == null;
@@ -54,17 +59,23 @@ public class AVLTree {
         } else {
             return null;
         }
-
+        // Update height in return phase
         node.height = treeHeight(node);
+
+        // Check bf and perform rotation if necessary
+        /**
+         * there will be 4 categories of rotation
+         * 1. LL rotation -> currentNode's balance_factor = 2 & currentNode.left's balance_factor = 1
+         * 2. RR rotation -> currentNode's balance_factor = -2 & currentNode.right's balance_factor = -1
+         * 3. LR rotation -> currentNode's balance_factor = 2 & currentNode.right's balance_factor = -1
+         * 4. RL rotation -> currentNode's balance_factor = -2 & currentNode.right's balance_factor = 1
+         */
+        if (getBalanceFactor(node) == 2 && getBalanceFactor(node.left) == 1) {//todo LL rotation logic }
+        if (getBalanceFactor(node) == -2 && getBalanceFactor(node.left) == -1) { //todo RR rotation logic }
+        if (getBalanceFactor(node) == 2 && getBalanceFactor(node.left) == -1) { // todo LR rotation logic }
+        if (getBalanceFactor(node) == -2 && getBalanceFactor(node.left) == 1) { // todo RL rotation logic }
+        return node;
     }
-
-    private TreeNode balancedTree() {
-
-    }
-
-//    public TreeNode insertRecursive(TreeNode node, int key) {
-//
-//    }
 
     public static void main(String[] args) {
     }
