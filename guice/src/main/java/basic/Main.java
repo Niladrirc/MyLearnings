615b9c3cd89106a6bda4f24fef475ba03af102fc
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-11 17:09:18 +0530

    Create Main.java

---
+++ guice/src/main/java/basic/Main.java
@@ -0,0 +1,14 @@
+package basic;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import discount_example.basic.guice.DiscountGuiceModule;
+
+public class Main {
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new DiscountGuiceModule());
+        CheckoutService service = injector.getInstance(CheckoutService.class);
+
+        service.checkout(100.00D);
+    }
+}
