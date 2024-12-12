20267377c9ab8cb302c56865072459ee03ce4932
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-12 11:32:38 +0530

    Create DAOModule.java

---
+++ learningjdbc/src/main/java/dao_layer/guice/DAOModule.java
@@ -0,0 +1,11 @@
+package dao_layer.guice;
+
+import com.google.inject.AbstractModule;
+
+public class DAOModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        super.configure();
+    }
+}
