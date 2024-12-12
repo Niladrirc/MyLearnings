40fc620240e7f2b1633cc296544ee77061fd4b36
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-12 11:47:48 +0530

    Update DepartmentDAO.java

--- learningjdbc/src/main/java/dao_layer/DepartmentDAO.java
+++ learningjdbc/src/main/java/dao_layer/DepartmentDAO.java
@@ -1,9 +1,11 @@
 package dao_layer;
 
 import com.google.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
 
+@Slf4j
 public class DepartmentDAO {
     private final Department department;
 
@@ -14,18 +16,43 @@ public class DepartmentDAO {
 
     public Department getDepartment(String departmentId) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees", "root", "Machine@123");
-            PreparedStatement st = conn.prepareStatement("SELECT * from `departments` where `dept_no`=?");
+            String query = "SELECT * from `departments` where `dept_no`=?";
+            Connection conn = getConnection();
+            PreparedStatement st = conn.prepareStatement(query));
             st.setString(1, departmentId);
-
             ResultSet rs = st.executeQuery();
             rs.next();
             department.setDepartmentId(rs.getString(1));
             department.setDepartmentName(rs.getString(2));
-
+            st.close(); conn.close();
             return department;
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int addDepartment(Department dept) {
+        String query = "INSERT INTO `departments` (dept_no, dept_name) VALUES (?, ?);";
+        int affectedRows;
+        try {
+            Connection conn = getConnection();
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setString(1, dept.getDepartmentId());
+            pst.setString(2, dept.getDepartmentName());
+
+            affectedRows = pst.executeUpdate();
+            log.info("{} rows affected", affectedRows);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return affectedRows;
+    }
+
+    private Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees", "root", "Machine@123");
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
