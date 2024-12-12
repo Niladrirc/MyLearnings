0273dd7cc7b9c1012587d5b67c458507a2fb11f8
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-12 12:52:28 +0530

    Update DAOModule.java

--- learningjdbc/src/main/java/dao_layer/guice/DAOModule.java
+++ learningjdbc/src/main/java/dao_layer/guice/DAOModule.java
@@ -1,11 +1,14 @@
 package dao_layer.guice;
 
 import com.google.inject.AbstractModule;
+import dao_layer.MyDataSource;
+
+import javax.sql.DataSource;
 
 public class DAOModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        super.configure();
+        bind(DataSource.class).to(MyDataSource.class);
     }
 }
