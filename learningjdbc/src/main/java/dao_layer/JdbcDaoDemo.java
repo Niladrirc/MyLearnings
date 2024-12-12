e82b82f1dcf62f2327cd06061ea7d170419b7243
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-12 11:32:41 +0530

    Create JdbcDaoDemo.java

---
+++ learningjdbc/src/main/java/dao_layer/JdbcDaoDemo.java
@@ -0,0 +1,33 @@
+package dao_layer;
+
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import dao_layer.guice.DAOModule;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@Slf4j
+public class JdbcDaoDemo {
+    private final DepartmentDAO deptAccessObj;
+
+    @Inject
+    public JdbcDaoDemo(DepartmentDAO deptAccessObj) {
+        this.deptAccessObj = deptAccessObj;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Injector injector = Guice.createInjector(new DAOModule());
+        JdbcDaoDemo demoApp = injector.getInstance(JdbcDaoDemo.class);
+        demoApp.run();
+    }
+
+    private void run() {
+        Department dept = deptAccessObj.getDepartment("d009");
+        log.info("DepartmentID: [{}] | DepartmentName: [{}]", dept.getDepartmentId(), dept.getDepartmentName());
+    }
+}
