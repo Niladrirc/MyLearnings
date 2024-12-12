a4377290607238b962696c6dd6d8eb942d09bb24
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-12 11:32:27 +0530

    Create Department.java

---
+++ learningjdbc/src/main/java/dao_layer/Department.java
@@ -0,0 +1,15 @@
+package dao_layer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Department {
+    private String departmentId;
+    private String departmentName;
+}
