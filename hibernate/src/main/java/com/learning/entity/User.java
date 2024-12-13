9cb9b85468505f28fee61934298638221627e7ef
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-13 19:47:39 +0530

    Create User.java

---
+++ hibernate/src/main/java/com/learning/entity/User.java
@@ -0,0 +1,32 @@
+package com.learning.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private Long phoneNumber;
+}
