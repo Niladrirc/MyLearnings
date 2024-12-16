3147b6402cf7ed80a2b7ca3b6b45d3484675e289
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-16 18:19:41 +0530

    Create CustomSerializationExample.java

---
+++ learning-serialization/src/CustomSerializationExample.java
@@ -0,0 +1,19 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class CustomSerializationExample {
+    public static void main(String[] args) throws Exception {
+        A a = new A();
+        FileOutputStream fos = new FileOutputStream("./A.json");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(a);
+
+        FileInputStream fis = new FileInputStream("./A.json");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        A a1 = (A) ois.readObject();
+
+        System.out.println(a1.getA() + " ... "+a1.getB());
+    }
+}
