4380b0e33583085c686ea541d146cf646d7b6600
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-16 18:27:39 +0530

    Update A.java

--- learning-serialization/src/A.java
+++ learning-serialization/src/A.java
@@ -2,13 +2,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.*;
 
-public class A implements Serializable {
+public class A {
 
     private int a=10;
     private int b=20;
 
     public A() {
         System.out.println("Inside non-parametrized constructor of A class");
+    }
+
+
+    public A(int a) {
+        System.out.println("Inside parametrized constructor of A class");
+        this.a = a;
     }
 
     public int getA() {
@@ -31,17 +37,17 @@ public class A implements Serializable {
         this.b = b;
     }
 
-    @Serial
-    private void writeObject(ObjectOutputStream oos) throws IOException {
-        oos.defaultWriteObject();
-        int c = a+20;
-        oos.writeInt(c);
-    }
-
-    @Serial
-    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
-        ois.defaultReadObject();
-        int c = ois.readInt();
-        System.out.println("Custom value c = "+c);
-    }
+//    @Serial
+//    private void writeObject(ObjectOutputStream oos) throws IOException {
+//        oos.defaultWriteObject();
+//        int c = a+20;
+//        oos.writeInt(c);
+//    }
+//
+//    @Serial
+//    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+//        ois.defaultReadObject();
+//        int c = ois.readInt();
+//        System.out.println("Custom value c = "+c);
+//    }
 }
