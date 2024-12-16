5f3f375c6b42d26afbc78505eeb72b5520f757ba
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-16 18:07:04 +0530

    Update Basic.java

--- learning-serialization/src/Basic.java
+++ learning-serialization/src/Basic.java
@@ -1,16 +1,18 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.*;
 
 public class Basic {
     public static void main(String[] args) throws Exception {
-        A a = new A();
+        B b = new B();
         FileOutputStream fos = new FileOutputStream("./A.json");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(a);
+        oos.writeObject(b);
 
         FileInputStream fis = new FileInputStream("./A.json");
         ObjectInputStream ois = new ObjectInputStream(fis);
-        A a1 = (A) ois.readObject();
+        B b1 = (B) ois.readObject();
 
-        System.out.println(a1.getA() + " ... "+a1.getB());
+        System.out.println(b1.getAObject().getA() + " ... "+b1.getAObject().getB());
     }
 }
