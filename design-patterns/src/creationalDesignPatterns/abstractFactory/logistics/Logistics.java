94e05634f24a81cb30a79c95a01df7e308802a60
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2024-12-30 13:23:38 +0530

    Update Logistics.java

--- design-patterns/src/creationalDesignPatterns/abstractFactory/logistics/Logistics.java
+++ design-patterns/src/creationalDesignPatterns/abstractFactory/logistics/Logistics.java
@@ -4,4 +4,9 @@ import creationalDesignPatterns.abstractFactory.transport.Transport;
 
 public abstract class Logistics {
     public abstract Transport createTransport();
+
+    // Primary task
+    public void someOperation() {
+        System.out.println("Doing some operation");
+    }
 }
