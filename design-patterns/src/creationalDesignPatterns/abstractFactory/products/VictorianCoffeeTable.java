270a18003ea33c4b637553ddd1b32fb843f2d4e6
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-01 13:26:27 +0530

    Delete VictorianCoffeeTable.java

--- design-patterns/src/creationalDesignPatterns/abstractFactory/products/VictorianCoffeeTable.java
+++
@@ -1,8 +0,0 @@
-package creationalDesignPatterns.abstractFactory.products;
-
-public class VictorianCoffeeTable implements CoffeeTable {
-    @Override
-    public void design() {
-        System.out.println("Victorian Coffee table design");
-    }
-}
