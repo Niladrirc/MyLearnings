f90c00e9e229fc45fad8db237fcc9fa23a0a8570
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-01 13:26:39 +0530

    Create VictorianCoffeeTable.java

---
+++ design-patterns/src/creationalDesignPatterns/abstractFactory/VictorianCoffeeTable.java
@@ -0,0 +1,8 @@
+package creationalDesignPatterns.abstractFactory;
+
+public class VictorianCoffeeTable implements CoffeeTable {
+    @Override
+    public void design() {
+        System.out.println("Victorian Coffee table design");
+    }
+}
