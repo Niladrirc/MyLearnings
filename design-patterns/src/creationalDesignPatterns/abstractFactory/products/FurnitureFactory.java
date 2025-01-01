0c506fb9cea5fa5f41a2fbd17289d863b37a52fd
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-01 13:26:20 +0530

    Delete FurnitureFactory.java

--- design-patterns/src/creationalDesignPatterns/abstractFactory/products/FurnitureFactory.java
+++
@@ -1,7 +0,0 @@
-package creationalDesignPatterns.abstractFactory.products;
-
-public interface FurnitureFactory {
-    Chair createChair();
-    Sofa createSofa();
-    CoffeeTable createCoffeeTable();
-}
