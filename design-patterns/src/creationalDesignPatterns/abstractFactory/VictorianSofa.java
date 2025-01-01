45bdfc34af509828d03d74bec3d3e2bcf0a63329
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-01 13:26:45 +0530

    Create VictorianSofa.java

---
+++ design-patterns/src/creationalDesignPatterns/abstractFactory/VictorianSofa.java
@@ -0,0 +1,13 @@
+package creationalDesignPatterns.abstractFactory;
+
+public class VictorianSofa implements Sofa {
+    @Override
+    public void numberOfSeats() {
+        System.out.println("3 seater victorian sofa");
+    }
+
+    @Override
+    public void sitOn() {
+        System.out.println("Can be sit on");
+    }
+}
