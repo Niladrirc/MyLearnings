f7917add9f0ec49efaddc0148ec6a41579dd7ea3
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-04 11:49:25 +0530

    Delete FileComponent.java

--- design-patterns/src/structuralDesignPatterns/composite/FileComponent.java
+++
@@ -1,9 +0,0 @@
-package structuralDesignPatterns.composite;
-
-public interface FileComponent {
-    void setAsReadable(boolean readable);
-    void setAdWriteable(boolean writeable);
-    void setAsExecutable(boolean executable);
-    void chmod(String pattern);
-    void ls();
-}
