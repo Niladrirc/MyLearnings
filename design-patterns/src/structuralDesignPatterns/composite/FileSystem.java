35c6fd91f15292eb5a038eedd40053cc7aaf9886
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-04 11:48:01 +0530

    Create FileSystem.java

---
+++ design-patterns/src/structuralDesignPatterns/composite/FileSystem.java
@@ -0,0 +1,8 @@
+package structuralDesignPatterns.composite;
+
+public class FileSystem {
+    public static void main(String[] args) {
+        File file = new File("Niladri");
+        file.ls();
+    }
+}
