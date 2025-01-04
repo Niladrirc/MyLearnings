df4390df50798fe67fe187ff030b2cc52cd60898
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-04 11:49:20 +0530

    Create FileSystemTest.java

---
+++ design-patterns/src/structuralDesignPatterns/composite/FileSystemTest.java
@@ -0,0 +1,8 @@
+package structuralDesignPatterns.composite;
+
+public class FileSystemTest {
+    public static void main(String[] args) {
+        File file = new File("Niladri");
+        file.ls();
+    }
+}
