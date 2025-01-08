88426bef7742b2d42a0410c8a10ba8228e6a2bf5
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-08 22:37:36 +0530

    Create Volts.java

---
+++ design-patterns/src/structuralDesignPatterns/adapter/Volts.java
@@ -0,0 +1,17 @@
+package structuralDesignPatterns.adapter;
+
+public class Volts {
+    private int volts;
+
+    public Volts(int volts) {
+        this.volts = volts;
+    }
+
+    public int getVolts() {
+        return volts;
+    }
+
+    public void setVolts(int volts) {
+        this.volts = volts;
+    }
+}
