76f2b8c0dd9cff00743e8361b25669de3f29b31e
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-16 14:59:57 +0530

    Create Combination.java

---
+++ DSA & CP/DSA/1.1.1: Data Structure/4: Recursion/JavaRecursion/src/combinations/Combination.java
@@ -0,0 +1,21 @@
+package combinations;
+
+import java.util.Scanner;
+
+public class Combination {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter r: ");
+        int r = sc.nextInt();
+
+
+        Naive naiveApproach = new Naive();
+        long result = naiveApproach.result(n,r);
+
+        System.out.println("Result: " + result);
+    }
+}
