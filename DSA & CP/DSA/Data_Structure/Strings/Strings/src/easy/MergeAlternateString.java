97a3ab6b6df89ff2b3a7fcffc75a521d8cdacf54
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-17 11:42:53 +0530

    Update MergeAlternateString.java

--- DSA & CP/DSA/Data_Structure/Strings/Strings/src/easy/MergeAlternateString.java
+++ DSA & CP/DSA/Data_Structure/Strings/Strings/src/easy/MergeAlternateString.java
@@ -1,6 +1,6 @@
 package easy;
 
-public class MergeALternateString {
+public class MergeAlternateString {
     public static void main(String[] args) {
         String word1 = "abcd";
         String word2 = "pqrst";
