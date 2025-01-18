567749fdef117a50815e57f28a5724b804812904
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-18 12:28:56 +0530

    Update FilterHotelTest.java

--- lambdas/src/passingFunctionsAsParametersWithoutLambdas/FilterHotelTest.java
+++ lambdas/src/passingFunctionsAsParametersWithoutLambdas/FilterHotelTest.java
@@ -20,6 +20,11 @@ public class FilterHotelTest {
          * 2. Instantiate a class implementing the interface
          * 3. implement the method of the interface, where the method body = function body which you want to pass as paramater
          * 4. pass an object of concrete implementation of the interface
+         *
+         * basically if you want to pass functions as method arguments then,
+         * wrap it inside the interface
+         * because, functions cannot be directly passed as arguments but objects can
+         * we just wrap the function with the interface so that it can be passed as method argument
          */
     }
 }
