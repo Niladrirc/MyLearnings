eea56b1247a139babb84b4d7ffdcefb4c95d0fe4
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-18 12:26:41 +0530

    Create FilterHotelTest.java

---
+++ lambdas/src/passingFunctionsAsParametersWithoutLambdas/FilterHotelTest.java
@@ -0,0 +1,25 @@
+package passingFunctionsAsParametersWithoutLambdas;
+
+import java.util.List;
+
+public class FilterHotelTest {
+    public static void main(String[] args) {
+        HotelService hotelService = new HotelService();
+
+        // Filter hotels with pricePerNight <= 2000
+        List<Hotel> hotels = hotelService.filterHotels(new HotelsLessThan2000());
+        System.out.println(hotels);
+
+        // Filter 5 star hotels
+        List<Hotel> fiveStarHotels = hotelService.filterHotels(new FiveStarHotels());
+        System.out.println(fiveStarHotels);
+
+        /**
+         * How to pass functions as method arguments or parameters:
+         * 1. Create an interface
+         * 2. Instantiate a class implementing the interface
+         * 3. implement the method of the interface, where the method body = function body which you want to pass as paramater
+         * 4. pass an object of concrete implementation of the interface
+         */
+    }
+}
