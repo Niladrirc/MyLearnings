2047e32fa66ee1df18014b223c11fd151bd2e320
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-18 15:16:02 +0530

    Update IFilterHotel.java

--- lambdas/src/passingFunctionsAsParametersWithoutLambdas/IFilterHotel.java
+++ lambdas/src/passingFunctionsAsParametersWithoutLambdas/IFilterHotel.java
@@ -3,4 +3,13 @@ package passingFunctionsAsParametersWithoutLambdas;
 @FunctionalInterface
 public interface IFilterHotel {
     boolean doFilter(Hotel hotel);
+
+    /**
+     * this interface can in no way under any given circumstance have more than 1 method.
+     *
+     * If there is then:
+     *      will fail for lambdas
+     *      can be made to work with other cases like concrete implementing class and anonymous inner classes
+     * Having the @FunctionalInterface is always advisable
+     */
 }
