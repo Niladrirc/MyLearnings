0f46398afb9986fbe524a883d14cf4ef34d45ba9
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-18 12:41:13 +0530

    Delete HotelsLessThan2000.java

--- lambdas/src/passingFunctionsAsParametersWithoutLambdas/HotelsLessThan2000.java
+++
@@ -1,8 +0,0 @@
-package passingFunctionsAsParametersWithoutLambdas;
-
-public class HotelsLessThan2000 implements IFilterHotel {
-    @Override
-    public boolean doFilter(Hotel hotel) {
-        return hotel.getPricePerNight() <= 2000;
-    }
-}
