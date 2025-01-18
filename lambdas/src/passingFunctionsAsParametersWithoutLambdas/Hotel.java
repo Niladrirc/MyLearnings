62a240f76cec6b5190be0bc2173947511f38cf7f
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-18 12:26:47 +0530

    Create Hotel.java

---
+++ lambdas/src/passingFunctionsAsParametersWithoutLambdas/Hotel.java
@@ -0,0 +1,48 @@
+package passingFunctionsAsParametersWithoutLambdas;
+
+
+public class Hotel {
+    private double pricePerNight;
+    private double rating;
+    private HotelType type;
+
+    public Hotel(double pricePerNight, double rating, HotelType type) {
+        this.pricePerNight = pricePerNight;
+        this.rating = rating;
+        this.type = type;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public HotelType getType() {
+        return type;
+    }
+
+    public void setType(HotelType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel: " +
+                "{" +
+                "pricePerNight=" + pricePerNight +
+                ", rating=" + rating +
+                ", type=" + type +
+                "}";
+    }
+}
