f8d8ff755d16ee07cc52b22eec22cb7eb4d47e9c
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-19 12:02:04 +0530

    Update Learning.java

--- DSA & CP/DSA/Data_Structure/Linked List/src/learn/Learning.java
+++ DSA & CP/DSA/Data_Structure/Linked List/src/learn/Learning.java
@@ -1,23 +1,34 @@
 package src.learn;
 
+import java.util.Iterator;
+
 public class Learning {
     public static void main(String[] args) {
-        DoublyLinkedList dll = new DoublyLinkedList();
+//        DoublyLinkedList dll = new DoublyLinkedList();
+        ListADT<Integer> list = new ListADT<>();
 
-        dll.add(0);
-        dll.add(1);
-        dll.add(2);
-        dll.add(3);
-        dll.add(4);
-        dll.add(5);
+        list.add(1);
+        list.add(2);
+        list.add(5);
 
-        dll.display();
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
 
-        dll.remove(0);
-        dll.display();
-
-        dll.reverse();
-        dll.display();
+//        dll.add(0);
+//        dll.add(1);
+//        dll.add(2);
+//        dll.add(3);
+//        dll.add(4);
+//        dll.add(5);
+//
+//        dll.display();
+//
+//        dll.remove(0);
+//        dll.display();
+//
+//        dll.reverse();
+//        dll.display();
 
     }
 }
