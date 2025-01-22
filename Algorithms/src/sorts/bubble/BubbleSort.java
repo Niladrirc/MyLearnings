d1639e747da0d5369f654103aa2137eedcc2a67d
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-01-22 12:59:52 +0530

    Update BubbleSort.java

--- Algorithms/src/sorts/bubble/BubbleSort.java
+++ Algorithms/src/sorts/bubble/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] A = {8,5,7,3,2};
+        int[] A = {8,5,7,3,10};
         sort(A);
         System.out.println(Arrays.toString(A));
     }
@@ -13,10 +13,10 @@ public class BubbleSort {
         int n = a.length;
         boolean swapped;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             swapped = false;
-            for (int j = i; j > 0; j--) {
-                if (a[j] < a[j + 1]) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (a[j] > a[j + 1]) {
                     int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
