821c185d1473fe35eb0b985d624cfc973cc0e53d
Niladri Choudhury <niladri.raychoudhury@gmail.com>
2025-02-08 09:22:54 +0530

    Create Student.java

---
+++ immutability_learing/src/Student.java
@@ -0,0 +1,33 @@
+public class Student {
+    private String name;
+    private String course;
+
+    public Student(String name, String course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: { " +
+                "name= " + this.name +
+                ", course= " + course +
+                " }";
+    }
+}
