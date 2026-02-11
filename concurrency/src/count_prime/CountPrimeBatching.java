d0389f10e7336ad7654a7e9b6173f0455f55ce56
RAY CHOUDHURY, NILADRI (ext) (FT D AA IN SGI DET CDF) <niladri.ray-choudhury.ext@siemens.com>
2026-02-11 16:01:22 +0530

    Update CountPrimeBatching.java

--- concurrency/src/count_prime/CountPrimeBatching.java
+++ concurrency/src/count_prime/CountPrimeBatching.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CountPrimeBatching {
