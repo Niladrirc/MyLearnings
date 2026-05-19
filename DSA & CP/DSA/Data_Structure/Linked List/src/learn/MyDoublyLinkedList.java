a472500bea22d1faaec80abd6dc05b566703cc4e
Niladri Ray Choudhury <67802649+Niladrirc@users.noreply.github.com>
2026-05-19 16:07:19 +0000

    access methods

--- DSA & CP/DSA/Data_Structure/Linked List/src/learn/MyDoublyLinkedList.java
+++ DSA & CP/DSA/Data_Structure/Linked List/src/learn/MyDoublyLinkedList.java
@@ -12,4 +12,40 @@ public class MyDoublyLinkedList<T> implements Iterable<T> {
         head = tail = new DoubleNode<>(data);
         size = 1;
     }
+
+    /* access methods */
+    public T getFirst() {
+        return head.getData();
+    }
+
+    public T getLast() {
+        return tail.getData();
+    }
+
+    public T get(int index) {
+        if (isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        }
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(
+                String.format("Index %s is out-of-bounds for list of size %s", index, size));
+        }
+
+        DoubleNode<T> node;
+        if (index < size/2) {
+            node = head;
+            for(int i = 0; i < index; i++) node = node.getNext();
+        } else {
+            node = tail;
+            for (int i = size-1; i > index; i--) node = node.getPrev();
+        }
+
+        return node.getData();
+
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
